291249cc82a16e48b1abcd112fa0d4cb3a0f989c
Ahmet Muzaffer Dülger <mdulger@gtu.edu.tr>
2017-12-19 01:22:33 +0300

    change algorithm and static methods

--- ParserClass.java
+++ ParserClass.java
@@ -7,69 +7,64 @@ import java.util.ArrayList;
 
 public class ParserClass {
 
-    public static class MethodPrinter {
+    public ArrayList<String> nameOfMethods = new ArrayList<String>();
+    public ArrayList<String> returnTypes = new ArrayList<String>();
+    public ArrayList<ArrayList<String>> parametersType = new ArrayList<ArrayList<String>>();
 
-        static ArrayList<String> nameOfMethods = new ArrayList<String>();
-        static ArrayList<String> returnTypes = new ArrayList<String>();
-        static ArrayList<ArrayList<String>> parametersType = new ArrayList<ArrayList<String>>();
+    public void myParser(String path) throws Exception {
 
-        public static void main(String[] args) throws Exception {
-            // creates an input stream for the file to be parsed
-            FileInputStream in = new FileInputStream("C:\\Users\\murat\\Desktop\\FitState.java");
+        FileInputStream in = new FileInputStream(path);
 
-            CompilationUnit cu;
-            try {
-                // parse the file
-                cu = JavaParser.parse(in);
-            } finally {
-                in.close();
-            }
-            // visit and print the methods names
-            cu.accept(new MethodVisitor(), null);
-
-            System.out.println(nameOfMethods);
-            System.out.println(returnTypes);
-            System.out.println(parametersType);
+        CompilationUnit cu;
+        try {
+            // parse the file
+            cu = com.github.javaparser.JavaParser.parse(in);
+        } finally {
+            in.close();
         }
+        // visit and print the methods names
+        cu.accept(new MethodVisitor(), null);
 
-        /**
-         * Simple visitor implementation for visiting MethodDeclaration nodes.
-         */
-        public static class MethodVisitor extends VoidVisitorAdapter {
+        System.out.println(nameOfMethods);
+        System.out.println(returnTypes);
+        System.out.println(parametersType);
 
-            @Override
-            public void visit(MethodDeclaration n, Object arg) {
-                // here you can access the attributes of the method.
-                // this method will be called for all methods in this
-                // CompilationUnit, including inner class methods
-                ArrayList<String> controlModifier = new ArrayList<String>();
-                ArrayList<String> subArrayList = new ArrayList<String>();
-
-                controlModifier.add(n.getModifiers().toString());
+    }
 
 
-                int controlIndex = controlModifier.get(0).indexOf(" ");
-                String controlModifierString = "";
-                if(controlIndex != -1)
-                    controlModifierString = controlModifier.get(0).substring(1,controlModifier.get(0).indexOf(" ")-1);
+    public class MethodVisitor extends VoidVisitorAdapter {
 
-                if(controlModifierString.equals("PRIVATE")) {
-                    ;
+        @Override
+        public void visit(MethodDeclaration n, Object arg) {
+            // here you can access the attributes of the method.
+            // this method will be called for all methods in this
+            // CompilationUnit, including inner class methods
+
+            ArrayList<String> controlModifier = new ArrayList<String>();
+            ArrayList<String> subArrayList = new ArrayList<String>();
+
+            controlModifier.add(n.getModifiers().toString());
+
+
+            int controlIndex = controlModifier.get(0).indexOf(" ");
+            String controlModifierString = "";
+            if(controlIndex != -1)
+                controlModifierString = controlModifier.get(0).substring(1,controlModifier.get(0).indexOf(" ")-1);
+
+            if(controlModifierString.equals("PRIVATE")) {
+                ;
+            }
+            else {
+
+                nameOfMethods.add(n.getName().toString());
+                returnTypes.add(n.getType().toString());
+                for(int i=0; i<n.getParameters().size(); i++){
+                    subArrayList.add(String.valueOf(n.getParameters().get(i)).substring(0, String.valueOf(n.getParameters().get(i)).indexOf(" ")));
                 }
-                else {
-
-                    nameOfMethods.add(n.getName().toString());
-                    returnTypes.add(n.getType().toString());
-                    for(int i=0; i<n.getParameters().size(); i++){
-                        subArrayList.add(String.valueOf(n.getParameters().get(i)).substring(0, String.valueOf(n.getParameters().get(i)).indexOf(" ")));
-                    }
-                    parametersType.add(subArrayList);
-
-                    /*System.out.println(n.getType());
-                    System.out.println(n.getName());
-                    System.out.println(n.getParameters());*/
-                }
+                parametersType.add(subArrayList);
             }
         }
     }
+
 }
+
